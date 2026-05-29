8e2e15971d14e04875e9f3cd15fc70de6e76e496
ferando59 <fernando_prado150@estudante.sesisenai.org.br>
2026-05-29 20:28:54 -0300

    no meio de trabalho criando as armas extras

---
+++ Assets/scripts/CopasGunsControler.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CopasGunsControler : MonoBehaviour
+{
+   public GameObject[] copasGuns;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
